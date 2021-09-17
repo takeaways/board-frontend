713f076ec5b41a57791153d33a93e28963c9b7f8
lucas.j <lucas.j@hpcnt.com>
2021-09-18 01:55:30 +0900

    material setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
-import "../styles/globals.css";
+import "styles/globals.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+
+import { themeDark, themeLight } from "lib/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // Remove the server-side injected css
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={true ? themeDark : themeLight}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,5 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
-const Home = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>gogo</title>
-        <meta name="keywords" content="gogome" />
-      </Head>
-    </div>
-  );
-};
+function Home() {
+  return <div>Hello World</div>;
+}
 
 export default Home;
